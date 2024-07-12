c305e2e11de254b95aa1f62524ede0aaf608d613
Kodekloner <billgateokoye@gmail.com>
2024-07-11 19:10:08 -0700

    updated 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,35 @@
-#!/usr/bin/python3
-""" Minimum Operations
+def minOperations(n):
+    """
+    Calculate the minimum number of operations needed
+    to get exactly n 'H' characters in a text file.
+
+    Operations allowed:
+    - Copy All
+    - Paste
+
+    Parameters:
+    n (int): The target number of 'H' characters
+
+    Returns:
+    int: The fewest number of operations needed to
+    achieve exactly n 'H' characters
+    If n is impossible to achieve, return 0
     """
 
+    if n <= 1:
+        return 0
 
-def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
     next = 'H'
     body = 'H'
-    op = 0
+    operations = 0
     while (len(body) < n):
         if n % len(body) == 0:
-            op += 2
+            operations += 2
             next = body
             body += body
         else:
-            op += 1
+            operations += 1
             body += next
     if len(body) != n:
         return 0
-    return op
+    return operations
